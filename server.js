0c97734a3e983e748cde0591c83ec970a3aed691
Kazu <kkondo@bullbearjapan.com>
2025-11-03 16:33:30 +0900

    Enable CORS for frontend

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
+app.use(cors({
+  origin: 'https://kondo-bbj.github.io' // Allow your frontend domain
+}));
 const port = 3000;
 
 app.use(express.json());
